f0860d3202317f75ee8810c134bcec45e0c09b1c
jakeTran42 <jake.tranehs@gmail.com>
2018-07-08 13:36:28 -0700

    add some session routes

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -5,6 +5,24 @@ const db = require('../server.js')
 
 module.exports = function(router) {
 
+    // Get sessions
+    router.get('theater/:theaterId/sessions', function(req, res) => {
+        db.find('sessions').then((sessions) => {
+            res.json({'stub': `[${req.originalUrl}]`})
+        }).catch((err) => {
+            res.send(err)
+        })
+    })
+
+    // Get session id
+    router.get('theater/:theaterId/sessions/:Id', function(req, res) => {
+        db.find('sessions', id).then((session) => {
+            res.json({'stub': `[${req.originalUrl}]`})
+        }).catch((err) => {
+            res.send(err)
+        }) 
+    })
+
     // Create a new session
     router.post('/theaters/:theaterId/sessions/new', function(req, res) {
         res.json({'stub': `[${req.originalUrl}] Endpoint works! Replace me in Step 2.`});
